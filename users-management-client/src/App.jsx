67116d0c8d771886fc4302b9c32662e07f4fc3bc
RAZU-sheikh <razu351724@gmail.com>
2023-10-13 00:10:19 +0600

    The form create and user taka informatio

--- users-management-client/src/App.jsx
+++ users-management-client/src/App.jsx
@@ -11,10 +11,33 @@ function App() {
     .then(data => setUsers(data))
   } ,[])
 
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user)
+  }
+
   return (
     <>
       <h1>Users Management System</h1>
       <h2>Numbers of Users: {users.length}</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
